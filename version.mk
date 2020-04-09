65ffb1471eb561ba3af855c597d9d48ffea10e12
Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>
2020-04-09 08:55:10 +0000

    Rename go.mk VERSION to _Version to prevent collision with VERSION in makefile
    Signed-off-by: Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>

--- version.mk
+++ version.mk
@@ -1,6 +1,6 @@
-VERSION := $(shell git describe --exact-match --tags $(git log -n1 --pretty='%h') 2> /dev/null | sed 's/^v//')
-ifndef VERSION
-    VERSION = dev
+_VERSION := $(shell git describe --exact-match --tags $(git log -n1 --pretty='%h') 2> /dev/null | sed 's/^v//')
+ifndef _VERSION
+    _VERSION = dev
 endif
 
 GIT_BRANCH := 	$(shell git rev-parse --abbrev-ref HEAD || echo 'n/a')
