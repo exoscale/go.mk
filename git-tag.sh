b2ac9f31f4a81359660835681285cbdb5ec24400
Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>
2020-04-21 09:58:21 +0200

    [git-tag] Support tag version without "v" suffix (#11)
    Signed-off-by: Pierre-Emmanuel Jacquier <15922119+pierre-emmanuelJ@users.noreply.github.com>

--- git-tag.sh
+++ git-tag.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
 validate() {
-    validation=$(echo "$1" | grep -oE '^v([0-9])\.([0-9])\.([0-9])$')
+    validation=$(echo "$1" | grep -oE '^v?([0-9])\.([0-9])\.([0-9])$')
     if [ -z "$validation" ]
     then
-        echo "error latest tag \"$1\" invalid format: expected vX.X.X"
-        exit 1
+        echo "Warning! Latest tag \"$1\" invalid format: expected vX.X.X or X.X.X"
+        printf "Enter your next tag manually: "
+        read -r manual_tag
+        git_tag "$manual_tag"
+        exit 0
     fi
 }
 
@@ -15,7 +18,7 @@ select_menu() {
         echo "1) $major - major"
         echo "2) $minor - minor"
         echo "3) $bugfix - bugfix"
-        printf "Choose tag to apply:"
+        printf "Choose tag to apply: "
         read -r choice
 
         if [ "$choice" = "1" ]
@@ -36,10 +39,40 @@ select_menu() {
     done
 }
 
+ask_v_versioning() {
+    echo "Latest tagged version: $tag"
+    printf "Would you like to add a \"v\" prefix to your next version tag? (y/N) "
+    read -r answer
+    if [ "$answer" = "y" ]
+    then
+        v="v"
+    fi
+}
+
+git_tag() {
+    printf "Describe your tag: "
+    read -r tag_description
+
+    if [ "$tag_description" = "" ]
+    then
+        tag_description="$1"
+    fi
+
+    git tag -a "$1" -m "$tag_description" && echo "Done! Tag successfuly applied"
+}
+
 tag=$(git describe --abbrev=0 --tags 2>/dev/null || echo v0.0.0)
 validate "$tag"
 
-tag=$(echo "$tag" | cut -c2-)
+v=""
+if [ "$(echo "$tag" | cut -c -1)" = "v" ]
+then
+    tag=$(echo "$tag" | cut -c2-)
+    v="v"
+else
+    ask_v_versioning
+fi
+
 
 # The only way to be sh cross platform between MacOS, Linux...etc
 # a.b.c represents a tag version e.g: "1.0.2"
@@ -48,13 +81,11 @@ tmp="${tag%??}"
 b="${tmp#??}"
 c="${tag#????}"
 
-major="v$((a+1)).0.0"
-minor="v$a.$((b+1)).0"
-bugfix="v$a.$b.$((c+1))"
+major="$v$((a+1)).0.0"
+minor="$v$a.$((b+1)).0"
+bugfix="$v$a.$b.$((c+1))"
 
 select_menu
 
-printf "Describe your tag:"
-read -r tag_description
+git_tag "$tag"
 
-git tag -a "$tag" -m "$tag_description" && echo "Done! Tag successfuly applied"
