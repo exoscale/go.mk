b00ab81141bfc8caa642d639d1f33f2571f2b5e4
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-06-26 16:24:06 +0200

    fix

--- do-release.sh
+++ do-release.sh
@@ -2,9 +2,15 @@
 
 set -e
 
-git clone git@github.com:sauterp/cli.git
+git clone https://github.com/sauterp/cli.git
+cd cli
 git checkout sauterp/sc-42833/cli-release
+
+# TODO(sauterp) remove this line
+git tag 1.70.1$BUILD_NUMBER
 git submodule update --init --recursive go.mk
+
+docker login --username philippsauterexoscale --password $DOCKERHUB_ACCESS_TOKEN
 
 export CGO_ENABLED=1
 make release
