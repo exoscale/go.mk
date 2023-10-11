aea4582256e9384b9a5e4f6bab0a15a037bcb357
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-10-11 18:13:59 +0200

    wip

--- scripts/publish-rpm-artifact-to-sos.sh
+++ scripts/publish-rpm-artifact-to-sos.sh
@@ -2,112 +2,62 @@
 
 # this script assumes, you are running it from the parent directory of this repository
 
-# set -e
+set -e
 
-# artifact=$1
-# bucketname=$2
-# repoprefix=$3
-# repoprefixescaped=$(echo $repoprefix | sed 's#/#\\\/#g')
-# if [ -z "$4" ]; then
-#     nrversionstokeep=10
-# else
-#     nrversionstokeep=$4
-# fi
+artifact=$1
+bucketname=$2
+repoprefix=$3
 
-# # Check if the artifact ends with ".deb"
-# if [ "${artifact%%.deb}" = "${artifact}" ]; then
-#     exit 0
-# fi
+# Check if the artifact ends with ".rpm"
+if [ "${artifact%%.rpm}" = "${artifact}" ]; then
+    exit 0
+fi
 
-# # The apt package manager only recognizes 'armhf' as a value for armv6 and armv7 packages.
-# # Since armv6 binaries will run on armv7 cpus, we only publish the armv6 package as the
-# # apt repo cannot have two packages for the 'same' armhf architecture
-# if [ "${artifact%%armv7.deb}" != "${artifact}" ]; then
-#     echo "not publishing armv7 package for debian in favor of armv6 package"
-#     exit 0
-# fi
+# goreleaser executes this script in parallel for each artifact but S3 repos can't be updated in parallel
+(
+    # Acquire an exclusive lock on the lock file, or wait until it's available
+    flock -w 1200 9 || exit 1
 
-# # goreleaser executes this script in parallel for each artifact but aptly repos can't be updated in parallel
-# (
-#     # Acquire an exclusive lock on the lock file, or wait until it's available
-#     flock -w 1200 9 || exit 1
+    if ! command -v createrepo_c || ! command -v rclone; then
+        sudo apt-get update
+        sudo apt-get install -y createrepo_c rclone
+    fi
 
-#     # install aptly if not available
-#     if ! command -v aptly; then
-#         sudo apt-get update
-#         sudo apt-get install -y aptly
-#     fi
+    # Get the 10 latest Git tags
+    # latest_tags=$(git tag --sort=-v:refname | head -n $nrversionstokeep)
 
-#     aptlyrepo=release-repo
-#     aptlyremote="s3:${bucketname}:"
-#     zone="ch-gva-2"
-#     archiveurl=https://sos-${zone}.exo.io/${bucketname}/${repoprefix}
-#     aptlymirror=${aptlyrepo}-mirror
-#     aptlydistro=stable
-#     aptlyconfig="go.mk/scripts/aptly.conf"
-#     aptlycmd="aptly -config=$aptlyconfig"
-#     gpgkeyflag='-gpg-key=7100E8BFD6199CE0374CB7F003686F8CDE378D41'
-#     archflag='-architectures=amd64,arm64,armhf'
+    # Create a package query filter for aptly
+    # package_filter=""
+    # for tag in $latest_tags; do
+    #     stripped_tag=$(expr "$tag" : '.\(.*\)')
+    #     if [ $first_tag_set ]; then
+    #         package_filter="${package_filter} | "
+    #     fi
+    #     package_filter="${package_filter} exoscale-cli (= ${stripped_tag})"
+    #     first_tag_set=1
+    # done
 
-#     # customize aptly.conf
-#     sed -e "s/PLACEHOLDER_FOR_BUCKETNAME/$bucketname/" \
-#         -e "s/PLACEHOLDER_FOR_ZONE/$zone/" \
-#         -e "s/PLACEHOLDER_FOR_PREFIX/${repoprefixescaped}/" ${aptlyconfig}.template >$aptlyconfig
+    # TODO (sc-78178) only keep last 10 versions
 
-#     # Get the 10 latest Git tags
-#     latest_tags=$(git tag --sort=-v:refname | head -n $nrversionstokeep)
+    reponame=rpmrepo
+    # TODO (sc-78178) remove sauterp
+    bucketname=sauterp-exoscale-packages
+    repodir=./rpmrepo/
+    rcloneconf=./rclone.config
+    rclonecmd="rclone --config=${rcloneconf}"
+    # TODO (sc-78178) customize
+    #     zone="ch-gva-2"
+    #     archiveurl=https://sos-${zone}.exo.io/${bucketname}/${repoprefix}
+    $rclonecmd config create $reponame s3 provider Other env_auth true endpoint sos-ch-gva-2.exo.io location_constraint ch-gva-2 acl public-read
 
-#     # Create a package query filter for aptly
-#     package_filter=""
-#     for tag in $latest_tags; do
-#         stripped_tag=$(expr "$tag" : '.\(.*\)')
-#         if [ $first_tag_set ]; then
-#             package_filter="${package_filter} | "
-#         fi
-#         package_filter="${package_filter} exoscale-cli (= ${stripped_tag})"
-#         first_tag_set=1
-#     done
+    mkdir -p $repodir
+    $rclonecmd sync -vv -P $reponame:${bucketname}/$repoprefix $repodir
 
-#     mirrorrepo() {
-#         $aptlycmd mirror create \
-#             $aptlymirror \
-#             $archiveurl \
-#             $aptlydistro
+    cp $artifact $repodir
+    createrepo_c $repodir
+    gpg --default-key=7100E8BFD6199CE0374CB7F003686F8CDE378D41 --detach-sign --armor ${repodir}/repodata/repomd.xml
 
-#         $aptlycmd mirror update \
-#             $aptlymirror
-#     }
+    $rclonecmd sync -vv -P $repodir $reponame:${bucketname}/$repoprefix
+) 9>/tmp/publish-rpm-artifact-to-sos.lock
 
-#     if ! $aptlycmd repo show $aptlyrepo 2>/dev/null; then
-#         isrepounpublished=1
-
-#         $aptlycmd repo create $aptlyrepo
-
-#         if mirrorrepo; then
-#             $aptlycmd repo import $aptlymirror $aptlyrepo "$package_filter"
-#         fi
-#     fi
-
-#     $aptlycmd repo add \
-#         $aptlyrepo \
-#         $artifact
-
-#     if [ $isrepounpublished ]; then
-#         $aptlycmd publish repo \
-#             $gpgkeyflag \
-#             $archflag \
-#             -distribution=$aptlydistro \
-#             $aptlyrepo \
-#             $aptlyremote
-#     fi
-
-#     # this step cleans up package files that are unreferenced
-#     $aptlycmd publish update \
-#         $gpgkeyflag \
-#         $archflag \
-#         $aptlydistro \
-#         $aptlyremote
-
-# ) 9>/tmp/publish-deb-artifact-to-sos.lock
-
-# rm -f /tmp/publish-deb-artifact-to-sos.lock
+rm -f /tmp/publish-rpm-artifact-to-sos.lock
