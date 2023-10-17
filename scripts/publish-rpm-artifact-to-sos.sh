845dbf77a53b11e8b799fe39284515e94c4a1e9e
Philipp Sauter <philipp.sauter@exoscale.ch>
2023-10-17 19:07:58 +0200

    fix

--- scripts/publish-rpm-artifact-to-sos.sh
+++ scripts/publish-rpm-artifact-to-sos.sh
@@ -73,6 +73,8 @@ fi
     # gpg --default-key=7100E8BFD6199CE0374CB7F003686F8CDE378D41 --detach-sign --armor $filetosign
 
     $rclonecmd sync -vv -P ${repodir} "${reponame}:${bucketname}/${repoprefix}"
+
+    sync
 ) 8>/tmp/publish-rpm-artifact-to-sos.lock
 
 rm -f /tmp/publish-rpm-artifact-to-sos.lock
